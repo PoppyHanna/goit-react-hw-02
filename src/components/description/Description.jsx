9279cecc78caa41395a0d8075e41b7d787d59866
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2024-03-26 00:59:54 -0400

    up 10

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types'; // Імпортуємо PropTypes
 import styles from './Description.module.css'
+import { IoIosCafe } from "react-icons/io";
 
 
 const Description = ({ title, text }) => {
     return (
         <div className={styles.description}>
-            <h1 className={styles.title}>{title}</h1>
+            <h1 className={styles.title}>{title}
+                <span className={styles.iconContainer}>
+                    <IoIosCafe className={styles.icon} />
+                </span>
+            </h1>
             <p className={styles.text}>{text}</p>
         </div>
     )
